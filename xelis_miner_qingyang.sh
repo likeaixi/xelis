88cc0e709b27ed98fca67a39a35f53f8222feb51
like <likeaixi@gmail.com>
2024-07-12 11:51:06 +0800

    update xelis miner

--- xelis_miner_qingyang.sh
+++ xelis_miner_qingyang.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-./onezerominer -a xelis -w xel:zc0tqcru5sc8ry23e3ry7qk908g9aeqtvlmgahedkzgmcmrcqgyqqpu54e3 -o ssl://eu.xel.k1pool.com:9352 --worker rig_name
-
 nohup /root/xelis/onezerominer -a xelis  -w xel:w2ta8au79jejkcjfqrd08jdm0hhcsuh3a3drrrdc3j5wtvxr7czsqeeateh -o 192.168.111.53:8080 --worker onezeroxelminer --xelis-solo >>/root/xelis/xelis_onezerominer-`date +%Y-%m-%d`.log  2>&1 &
 
 echo "[`date '+%Y-%m-%d %H:%M:%s'`] xelis_onezerominer started"
